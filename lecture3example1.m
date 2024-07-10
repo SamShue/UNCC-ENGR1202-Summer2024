c5bac569380691fe721cd322c1bd224cb38b7f18
Sam Shue <evilrobotfactory@gmail.com>
2024-07-10 10:03:13 -0400

    Create lecture3example1.m

---
+++ lecture3example1.m
@@ -0,0 +1,18 @@
+clc;
+clear all;
+close all;
+
+Fs = 84000;
+
+t = 0:5*(1/Fs):5; % seconds
+f1 = 5000;
+f2 = 200;
+A1 = 1;
+A2 = 0.5;
+
+y1 = A1*sin(t*2*pi*f1);
+y2 = A2*sin(t*2*pi*f2);
+
+y = y1 + y2;
+
+sound(y, Fs)
